973fe0f2c874aae858a17743478cfc4be13d4189
JbellMD <91991220+JbellMD@users.noreply.github.com>
2024-08-05 13:30:19 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,18 +8,18 @@ app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
     path('register', view=views.registration, name='register'),
-    
     # path for login
     path('login', view=views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    
     # other paths
     path('get_cars', view=views.get_cars, name='get_cars'),
     path('get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path('dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    
+    path('get_dealers/<str:state>', view=views.get_dealerships, 
+         name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', view=views.get_dealer_details, 
+         name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, 
+         name='dealer_details'),
     # path for add a review view
     path('add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
